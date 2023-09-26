85f5953263a69a5ee4e0114c688bdd6dd796d025
Mawuli Kudjawu <mawuli.kudjawu@outlook.com>
2023-09-25 21:22:55 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace SecOPs_security_suite
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.NotifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.ContextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.About = new System.Windows.Forms.ToolStripMenuItem();
+            this.Restore = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.Exit = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuStrip = new System.Windows.Forms.MenuStrip();
+            this.File = new System.Windows.Forms.ToolStripMenuItem();
+            this.UpdateSignatures = new System.Windows.Forms.ToolStripMenuItem();
+            this.SendReport = new System.Windows.Forms.ToolStripMenuItem();
+            this.Scandrives = new System.Windows.Forms.ToolStripMenuItem();
+            this.Scanurl = new System.Windows.Forms.ToolStripMenuItem();
+            this.Scanfile = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblComputerName = new System.Windows.Forms.Label();
+            this.labelInfo = new System.Windows.Forms.Label();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.ContextMenuStrip.SuspendLayout();
+            this.MenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // NotifyIcon
+            // 
+            this.NotifyIcon.ContextMenuStrip = this.ContextMenuStrip;
+            this.NotifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("NotifyIcon.Icon")));
+            this.NotifyIcon.Text = "SecOPs Security Suite";
+            this.NotifyIcon.Visible = true;
+            // 
+            // ContextMenuStrip
+            // 
+            this.ContextMenuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.ContextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.About,
+            this.Restore,
+            this.toolStripSeparator1,
+            this.Exit});
+            this.ContextMenuStrip.Name = "contextMenuStrip";
+            this.ContextMenuStrip.Size = new System.Drawing.Size(172, 82);
+            // 
+            // About
+            // 
+            this.About.Name = "About";
+            this.About.Size = new System.Drawing.Size(171, 24);
+            this.About.Text = "About SecOPs";
+            this.About.Click += new System.EventHandler(this.About_Click);
+            // 
+            // Restore
+            // 
+            this.Restore.Name = "Restore";
+            this.Restore.Size = new System.Drawing.Size(171, 24);
+            this.Restore.Text = "Restore";
+            this.Restore.Click += new System.EventHandler(this.Restore_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(168, 6);
+            // 
+            // Exit
+            // 
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(171, 24);
+            this.Exit.Text = "Exit";
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // MenuStrip
+            // 
+            this.MenuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.MenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.File});
+            this.MenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.MenuStrip.Name = "MenuStrip";
+            this.MenuStrip.Size = new System.Drawing.Size(800, 28);
+            this.MenuStrip.TabIndex = 1;
+            this.MenuStrip.Text = "menuStrip1";
+            // 
+            // File
+            // 
+            this.File.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.UpdateSignatures,
+            this.SendReport,
+            this.Scandrives,
+            this.Scanurl,
+            this.Scanfile});
+            this.File.Name = "File";
+            this.File.Size = new System.Drawing.Size(46, 24);
+            this.File.Text = "File";
+            // 
+            // UpdateSignatures
+            // 
+            this.UpdateSignatures.Name = "UpdateSignatures";
+            this.UpdateSignatures.Size = new System.Drawing.Size(214, 26);
+            this.UpdateSignatures.Text = "Update Signatures";
+            this.UpdateSignatures.Click += new System.EventHandler(this.UpdateSignatures_Click);
+            // 
+            // SendReport
+            // 
+            this.SendReport.Name = "SendReport";
+            this.SendReport.Size = new System.Drawing.Size(214, 26);
+            this.SendReport.Text = "Send Report...";
+            this.SendReport.Click += new System.EventHandler(this.SendReport_Click);
+            // 
+            // Scandrives
+            // 
+            this.Scandrives.Name = "Scandrives";
+            this.Scandrives.Size = new System.Drawing.Size(214, 26);
+            this.Scandrives.Text = "Scan drive(s)...";
+            this.Scandrives.Click += new System.EventHandler(this.Scandrives_Click);
+            // 
+            // Scanurl
+            // 
+            this.Scanurl.Name = "Scanurl";
+            this.Scanurl.Size = new System.Drawing.Size(214, 26);
+            this.Scanurl.Text = "Scan url\'s...";
+            this.Scanurl.Click += new System.EventHandler(this.Scanurl_Click);
+            // 
+            // Scanfile
+            // 
+            this.Scanfile.Name = "Scanfile";
+            this.Scanfile.Size = new System.Drawing.Size(214, 26);
+            this.Scanfile.Text = "Scan file(s)...";
+            this.Scanfile.Click += new System.EventHandler(this.Scanfile_Click);
+            // 
+            // lblComputerName
+            // 
+            this.lblComputerName.AutoSize = true;
+            this.lblComputerName.Location = new System.Drawing.Point(13, 40);
+            this.lblComputerName.Name = "lblComputerName";
+            this.lblComputerName.Size = new System.Drawing.Size(0, 18);
+            this.lblComputerName.TabIndex = 2;
+            // 
+            // labelInfo
+            // 
+            this.labelInfo.AutoSize = true;
+            this.labelInfo.Location = new System.Drawing.Point(13, 460);
+            this.labelInfo.Name = "labelInfo";
+            this.labelInfo.Size = new System.Drawing.Size(46, 18);
+            this.labelInfo.TabIndex = 3;
+            this.labelInfo.Text = "label1";
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.AllowNavigation = false;
+            this.webBrowser1.IsWebBrowserContextMenuEnabled = false;
+            this.webBrowser1.Location = new System.Drawing.Point(274, 40);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScriptErrorsSuppressed = true;
+            this.webBrowser1.ScrollBarsEnabled = false;
+            this.webBrowser1.Size = new System.Drawing.Size(514, 454);
+            this.webBrowser1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 506);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.labelInfo);
+            this.Controls.Add(this.lblComputerName);
+            this.Controls.Add(this.MenuStrip);
+            this.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.MenuStrip;
+            this.Name = "Form1";
+            this.Text = "SecOPs Security Suite";
+            this.ContextMenuStrip.ResumeLayout(false);
+            this.MenuStrip.ResumeLayout(false);
+            this.MenuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon NotifyIcon;
+        private new System.Windows.Forms.ContextMenuStrip ContextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem About;
+        private System.Windows.Forms.ToolStripMenuItem Restore;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem Exit;
+        private System.Windows.Forms.MenuStrip MenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem File;
+        private System.Windows.Forms.ToolStripMenuItem UpdateSignatures;
+        private System.Windows.Forms.ToolStripMenuItem SendReport;
+        private System.Windows.Forms.ToolStripMenuItem Scandrives;
+        private System.Windows.Forms.ToolStripMenuItem Scanurl;
+        private System.Windows.Forms.ToolStripMenuItem Scanfile;
+        private System.Windows.Forms.Label lblComputerName;
+        private System.Windows.Forms.Label labelInfo;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,136 @@
+﻿using Aspose.Cells.Drawing;
+using System;
+using System.Windows.Forms;
+using VirusTotalNet;
+
+namespace SecOPs_security_suite
+{
+    public partial class Form1 : Form
+    {
+
+
+        private readonly VirusTotal virusTotal;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            // Initialize the VirusTotal API client
+            VirusTotal virusTotal = new VirusTotal("720d9ff210ffff987fa962426fee12753766501b02ef4b21d30b5f10e3ac8a65");
+            this.virusTotal = virusTotal;
+            this.virusTotal.UseTLS = true;
+
+            string computerName = Environment.MachineName;
+
+            // Start a timer to update the date and time every second
+            Timer timer = new Timer();
+            timer.Interval = 1000; // 1 second
+            timer.Tick += Timer_Tick;
+            timer.Start();
+
+            //initialize webbrowser
+
+            LoadLocalHtmlFile("file:///C:/Users/mawul/source/repos/SecOPs%20security%20suite/index.html");
+
+        }
+
+        private void LoadLocalHtmlFile(string filePath)
+        {
+
+            string url = new Uri(filePath).AbsoluteUri;
+            webBrowser1.Navigate(url);
+
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+
+            // Update the label with the current date and time
+            labelInfo.Text = "Computer Name: " + Environment.MachineName + "\n" +
+                           "Date and Time: " + DateTime.Now.ToString();
+        }
+
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (FormWindowState.Minimized == WindowState)
+                Hide();
+        }
+
+        private void NotifyIcon_DoubleClick(object sender, EventArgs e)
+        {
+            Show();
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void About_Click(object sender, EventArgs e)
+        {
+            // Display a message box about the application.
+            MessageBox.Show("SecOPs Security Suite is a collectiton of Security tools for Organizations.", "About SecOPs", MessageBoxButtons.OK);
+        }
+
+        private void Restore_Click(object sender, EventArgs e)
+        {
+            Show();
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            // Display a message box asking the user to confirm that they want to exit.
+            DialogResult result = MessageBox.Show("Are you sure you want to exit?", "Exit", MessageBoxButtons.YesNo);
+
+            if (result == DialogResult.Yes)
+            {
+                // Close the application.
+                Application.Exit();
+            }
+        }
+
+        private void UpdateSignatures_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SendReport_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Scandrives_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Scanurl_Click(object sender, EventArgs e)
+        {
+            // Create a dialog box with a text box for the URL.
+            var dialog = new DialogBox();
+            dialog.Title = "Enter URL";
+            dialog.Controls.Add(new System.Windows.Forms.TextBox { Text = "Enter URL here" });
+
+            // Show the dialog box and get the URL.
+            var url = dialog.ShowDialog();
+
+            // If the user entered a URL, scan it.
+            if (url != null)
+            {
+                // Create a VirusTotal API client.
+                var client = new VirusTotalNetAPIClient("720d9ff210ffff987fa962426fee12753766501b02ef4b21d30b5f10e3ac8a65");
+
+                // Get the scan results for the URL.
+                var scanResults = client.ScanURL(url);
+
+                // Display the scan results in WebBrowser1.
+                webBrowser1.Navigate(scanResults.ToString());
+            }
+        }
+
+        private void Scanfile_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
